f7e5572cb709dc4bf6bf4e3e7cd9f9d2e93e214a
Mirel Mitache <mirel.mitache@avira.com>
2015-03-06 16:49:54 +0200

    Fixed for multiline expressions

--- form/fields/ForumTextarea.php
+++ form/fields/ForumTextarea.php
@@ -110,7 +110,7 @@ SCRIPT;
      * @return string
      */
     protected static function applyRule($text, $name, $rule){
-        preg_match_all("#\\[$name\\](.*?)\\[\\/$name\\]#s", $text, $matches);
+        preg_match_all("#\\[$name\\](.*?)\\[\\/$name\\]#sm", $text, $matches);
         $list = [];
         foreach ($matches[0] as $k=>$original){
             $list[$original] = $matches[1][$k]; // original => text
