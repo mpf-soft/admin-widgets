2deeb63b27d704759deb324c8e27bf87018ece84
Mirel Mitache <mirel.mitache@avira.com>
2016-10-18 12:14:21 +0300

    Allow code and color tags and option to add other tags later

--- form/fields/Markdown.php
+++ form/fields/Markdown.php
@@ -14,7 +14,15 @@ use mpf\web\helpers\Html;
 use mpf\WebApp;
 use mpf\widgets\form\Field;
 
-class Markdown extends Field {
+class Markdown extends Field
+{
+    public $stripTagsFor = ['#\[code=([a-zA-Z0-9_\-]+)\](.*?)\[/code\]#sm', '#\[code\](.*?)\[/code\]#sm'];
+
+    public $extraFilters = [
+        '#\[code=([a-zA-Z0-9_\-]+)\](.*?)\[/code\]#sm' => '<pre><code class="$1">$2</code></pre>',
+        '#\[code\](.*?)\[/code\]#sm' => '<pre><code>$1</code></pre>',
+        '#\[color=([\#a-zA-Z0-9_\-]+)\](.*?)\[/color\]#sm' => '<span style="color:$1;">$2</span>'
+    ];
 
     public $hintText = "This input uses Markdown syntax. <a href='https://daringfireball.net/projects/markdown/syntax' target='_blank'>Click For Details</a>";
 
@@ -28,7 +36,8 @@ class Markdown extends Field {
      * Get HTML Code for Input
      * @return string
      */
-    public function getInput() {
+    public function getInput()
+    {
         $this->htmlOptions['class'] = (isset($this->htmlOptions['class']) ? $this->htmlOptions['class'] . ' ' : '') . 'input markdown-input';
         $this->htmlOptions['ajax-url'] = $this->previewURL ?: WebApp::get()->request()->getCurrentURL();
         $this->htmlOptions['csrf-key'] = WebApp::get()->request()->getCsrfKey();
@@ -39,30 +48,57 @@ class Markdown extends Field {
     /**
      * @return string
      */
-    public function getHint() {
-        return Html::get()->tag("span", $this->hintText, ["class" => "markdown-hint"]);
+    public function getHint()
+    {
+        return Html::get()->tag("span", $this->translate($this->hintText), ["class" => "markdown-hint"]);
     }
 
     /**
      * @return string
      */
-    public function getPreview() {
+    public function getPreview()
+    {
         return Html::get()->tag("div", "", ["class" => "markdown-preview"]);
+    }
+
+    /**
+     * @param string $t
+     * @return string
+     */
+    protected static function processTextFilters($t)
+    {
+        $extra = new self();
+        $strip = $extra->stripTagsFor;
+        $extra = $extra->extraFilters;
+        foreach ($strip as $regexp) {
+            preg_match_all($regexp, $t, $matches);
+            foreach ($matches as $match) {
+                if ($match) {
+                    $t = str_replace($match[0], strip_tags($match[0]), $t);
+                }
+            }
+        }
+        foreach ($extra as $regexp => $replace) {
+            $t = preg_replace($regexp, $replace, $t);
+        }
+        return $t;
     }
 
     /**
      * @param string $original
      * @return string
      */
-    public static function processText($original) {
-        return \Michelf\Markdown::defaultTransform($original);
+    public static function processText($original)
+    {
+        return self::processTextFilters(\Michelf\Markdown::defaultTransform($original));
     }
 
     /**
      * Overwrite field getContent to add hints
      * @return string
      */
-    public function getContent() {
+    public function getContent()
+    {
         return parent::getContent() . $this->getHint() . $this->getPreview();
     }
 }
